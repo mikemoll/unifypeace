c8c735d7d69a313552eeff3f9554945a5635ab66
bentang <bentang@41studio.com>
2014-09-01 13:23:32 +0700

    add gemfile activadmin

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,8 @@
 class Event < ActiveRecord::Base
-
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+  has_and_belongs_to_many :categories
   with_options dependent: :destroy do |relation|
-    relation.has_many :categories
     relation.has_many :affiliated_organizations
   end
 end

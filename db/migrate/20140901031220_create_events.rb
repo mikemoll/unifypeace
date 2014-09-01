0f5dab78b36907fce44007b93eb141b5717596c7
Fitria Rahmaani <fitria@41studio.com>
2014-09-01 13:09:01 +0700

    Fixed event

--- db/migrate/20140901031220_create_events.rb
+++ db/migrate/20140901031220_create_events.rb
@@ -10,6 +10,10 @@ class CreateEvents < ActiveRecord::Migration
       t.string :organizer_name
       t.string :organizer_email
       t.integer :affiliated_organization_id
+      t.string    :country
+      t.string    :region
+      t.string    :city
+      t.string    :postal_code
       t.float :lat
       t.float :long
       t.string :status, default: "pending"

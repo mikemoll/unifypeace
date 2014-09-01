2ea6f24ca4103ac82b6e4dc09f2b2449584914ee
bentang <bentang@41studio.com>
2014-09-01 13:35:51 +0700

    delete conflict

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -9,4 +8,5 @@
 #
 Category.destroy_all
 Category.create([{name: "meditation/prayer"}, {name: "music/celebration"}, {name: "march/action"}])
+AffiliatedOrganization.destroy_all
 AffiliatedOrganization.create([{name: "Giant Mind"}, {name: "United Religions Initiative"}, {name: "BeThePeace​"}, {name: "Pathways to Peace​"}, {name: "International Day of Peace​"}, {name: "Summer of Peace​"}, {name: "World Peace and Prayer Society​"}, {name: "We.net​"}, {name: "Compassion Games​"}, {name: "Uplift​​​"}, {name: "Peace One Day​"}, {name: "Do As One​"}, {name: "Chopra Center​"}, {name: "Earth Dance ​"}, {name: "Pachamama Alliance​"}, {name: "Master Shift ​"}, {name: "MedMob​"}, {name: "One World Still​"}, {name: "Art of Living​"}, {name: "Oneness University​"}, {name: "Humanity's Team​"}])

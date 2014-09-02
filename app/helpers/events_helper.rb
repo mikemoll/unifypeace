685339b125908cc5c5c415a8a4d69a5c723bca34
bentang <bentang@41studio.com>
2014-09-02 20:19:05 +0700

    options for select

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,5 @@
 module EventsHelper
-  def options_for_attendees
-    options_for_select([["1-10", "1-10"], ["10-50", "10-50"], ["50-200", "50-200"], ["200-500", "200-500"] , ["500+", "500+"]])
+  def options_for_attendees(event)
+    options_for_select([["1-10", "1-10"], ["10-50", "10-50"], ["50-200", "50-200"], ["200-500", "200-500"] , ["500+", "500+"]], (event.estimated_attendees if event) )
   end
-
 end

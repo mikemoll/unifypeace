4c327a8d88571711de10d531cf5c3221a2143184
bentang <bentang@41studio.com>
2014-09-03 20:26:48 +0700

    change design map in event details

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -18,7 +18,11 @@ class Devise::SessionsController < DeviseController
     set_flash_message(:notice, :signed_in) if is_flashing_format?
     sign_in(resource_name, resource)
     yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
+    if params[:slug].blank?
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      redirect_to event_path(params[:slug])
+    end
   end
 
   # DELETE /resource/sign_out
